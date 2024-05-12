c47e98dd49fb0e3fbe3d887df34bbc29445dc676
Harshaiag <hsm23@student.le.ac.uk>
2024-05-12 21:39:23 +0100

    Added one last feature in the unique feature

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-const ham = document.querySelector('.ham-menu')
-const nav = document.querySelector('.navbar')
-
-ham.addEventListener("click",()=>{
-    ham.classList.toggle('active')
-    nav.classList.toggle('active')
-    
-})
-
 
 
 
